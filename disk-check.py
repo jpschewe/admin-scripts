cfc3612b18c67f6fb6e5f4a2a96ac319be6f80d6
Jon Schewe <jpschewe@mtu.net>
2012-05-07 16:06:08 -0500

    Add ability to ignore mounts and check for disk space by volume as well as percentage

--- disk-check.py
+++ disk-check.py
@@ -2,24 +2,30 @@
 
 import os
 import commands
+import re
 
 high_water_mark = 0.95
+# in bytes
+minimum_disk_space = 5000000000
 
-output = commands.getoutput('df -P')
+output = commands.getoutput('/bin/df -P  2>/dev/null')
+ignored_mounts = [ '/kern', '/dev/pts', '/proc', '/media', '/archive' ]
+
 lines = output.split('\n')
 
-points = map(lambda line: " ".join(line.split()[5:]), lines)
-    
-for mount in points:
-    #print "Checking mount '%s'" % (mount)
-    if 'Mounted on' != mount:
+for line in lines:
+    pieces = line.split()
+    dev = pieces[0]
+    mount = pieces[5]
+    #print "Checking mount '%s' dev '%s'" % (mount, dev)
+    if 'Mounted' != mount and mount not in ignored_mounts and not re.search(r':', dev) :
         disk = os.statvfs(mount)
         capacity = disk.f_bsize * disk.f_blocks
         available = disk.f_bsize * disk.f_bavail
         used = disk.f_bsize * (disk.f_blocks - disk.f_bavail)
         percent_used = float(used)/float(capacity)
         
-        if percent_used > high_water_mark:
-            print("Filesystem %s is close to full: %s%%" % (mount, percent_used * 100))
+        if percent_used > high_water_mark and available < minimum_disk_space:
+            print("Filesystem %s is close to full: %s%% avail: %sG" % (mount, percent_used * 100, available / 1024 / 1024 / 1024))
         #else:
         #    print("Filesystem %s percent used is %s" % (mount, percent_used * 100))
