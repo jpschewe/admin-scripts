1dbac0c17b651d43e4a51ee34c417848516e47cb
Jon Schewe <jpschewe@mtu.net>
2023-05-19 17:24:48 -0500

    Update disk-check for python3

--- disk-check.py
+++ disk-check.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
-import commands
+import subprocess
 import re
 
 high_water_mark = 0.95
 # in bytes
 minimum_disk_space = 5000000000
 
-output = commands.getoutput('/bin/df -P  2>/dev/null')
+output = subprocess.getoutput('/bin/df -P  2>/dev/null')
 ignored_mounts = [ '/kern', '/dev/pts', '/proc', '/media', '/archive' ]
 
 lines = output.split('\n')
@@ -18,7 +18,7 @@ for line in lines:
     dev = pieces[0]
     mount = pieces[5]
     #print "Checking mount '%s' dev '%s'" % (mount, dev)
-    if 'Mounted' != mount and mount not in ignored_mounts and not re.search(r':', dev) :
+    if 'Mounted' != mount and mount not in ignored_mounts and not re.search(r':', dev) and not mount.startswith('/snap') :
         disk = os.statvfs(mount)
         capacity = disk.f_bsize * disk.f_blocks
         available = disk.f_bsize * disk.f_bavail
